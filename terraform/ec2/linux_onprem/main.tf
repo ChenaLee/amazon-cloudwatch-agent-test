ed9d718f444783917cf2fd3993d80cc36bd7dd53
Chena Lee <leechena@amazon.com>
2023-06-22 13:36:00 -0400

    pull out common module for linux ec2

--- terraform/ec2/linux_onprem/main.tf
+++ terraform/ec2/linux_onprem/main.tf
@@ -1,165 +1,38 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 
-module "common" {
-  source = "../../common"
-}
-
-module "basic_components" {
-  source = "../../basic_components"
+module "linux_common" {
+  source = "../linux_common"
 
   region = var.region
-}
-
-#####################################################################
-# Generate EC2 Key Pair for log in access to EC2
-#####################################################################
-
-resource "tls_private_key" "ssh_key" {
-  count     = var.ssh_key_name == "" ? 1 : 0
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "aws_ssh_key" {
-  count      = var.ssh_key_name == "" ? 1 : 0
-  key_name   = "ec2-key-pair-${module.common.testing_id}"
-  public_key = tls_private_key.ssh_key[0].public_key_openssh
+  ec2_instance_type = var.ec2_instance_type
+  ssh_key_name = var.ssh_key_name
+  ami = var.ami
+  ssh_key_value = var.ssh_key_value
+  user = var.user
+  arc = var.arc
+  test_name = var.test_name
+  test_dir = var.test_dir
+  is_canary = var.is_canary
 }
 
 locals {
-  ssh_key_name        = var.ssh_key_name != "" ? var.ssh_key_name : aws_key_pair.aws_ssh_key[0].key_name
-  private_key_content = var.ssh_key_name != "" ? var.ssh_key_value : tls_private_key.ssh_key[0].private_key_pem
-  // Canary downloads latest binary. Integration test downloads binary connect to git hash.
-  binary_uri = var.is_canary ? "${var.s3_bucket}/release/amazon_linux/${var.arc}/latest/${var.binary_name}" : "${var.s3_bucket}/integration-test/binary/${var.cwa_github_sha}/linux/${var.arc}/${var.binary_name}"
   // list of test that require instance reboot
   reboot_required_tests = tolist(["./test/restart"])
-}
-
-# create a proxy instance for tests using proxy instsance
-module "proxy_instance" {
-  source              = "../proxy"
-  create              = length(regexall("proxy", var.test_dir)) > 0 ? 1 : 0
-  ssh_key_name        = local.ssh_key_name
-  private_key_content = local.private_key_content
-  test_dir            = var.test_dir
-  test_name           = var.test_name
-  region              = var.region
-  user                = var.user
+  // Canary downloads latest binary. Integration test downloads binary connect to git hash.
+  binary_uri = var.is_canary ? "${var.s3_bucket}/release/amazon_linux/${var.arc}/latest/${var.binary_name}" : "${var.s3_bucket}/integration-test/binary/${var.cwa_github_sha}/linux/${var.arc}/${var.binary_name}"
 }
 
 #####################################################################
-# Generate EC2 Instance and execute test commands
+# Execute test
 #####################################################################
-resource "aws_instance" "cwagent" {
-  ami                                  = data.aws_ami.latest.id
-  instance_type                        = var.ec2_instance_type
-  key_name                             = local.ssh_key_name
-  iam_instance_profile                 = module.basic_components.instance_profile
-  vpc_security_group_ids               = [module.basic_components.security_group]
-  associate_public_ip_address          = true
-  instance_initiated_shutdown_behavior = "terminate"
-
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-
-  tags = {
-    Name = var.is_canary ? "cwagent-canary-test-ec2-${var.test_name}-${module.common.testing_id}" : "cwagent-integ-test-ec2-${var.test_name}-${module.common.testing_id}"
-  }
-}
-
-resource "null_resource" "integration_test_fips_setup" {
-  # run go test when it's not feature test
-  count = length(regexall("fips", var.test_dir)) > 0 ? 1 : 0
-
-  connection {
-    type        = "ssh"
-    user        = var.user
-    private_key = local.private_key_content
-    host        = aws_instance.cwagent.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo enabling fips",
-      "sudo yum install -y dracut-fips",
-      "sudo dracut -f",
-      "sudo /sbin/grubby --update-kernel=ALL --args=\"fips=1\"",
-      "sudo reboot &",
-    ]
-  }
-
-  depends_on = [
-    aws_instance.cwagent,
-  ]
-}
-
-resource "null_resource" "integration_test_fips_check" {
-  # run go test when it's not feature test
-  count = length(regexall("fips", var.test_dir)) > 0 ? 1 : 0
-
-  connection {
-    type        = "ssh"
-    user        = var.user
-    private_key = local.private_key_content
-    host        = aws_instance.cwagent.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo `cat /proc/sys/crypto/fips_enabled`",
-      "echo `sysctl crypto.fips_enabled`",
-      "if [ `cat /proc/sys/crypto/fips_enabled` -ne \"1\" ];then echo \"FIPS is not enabled, please check file /proc/sys/crypto/fips_enabled.\";exit 1; fi",
-    ]
-  }
-
-  depends_on = [
-    null_resource.integration_test_fips_setup,
-  ]
-}
-
-## reboot when only needed
-resource "null_resource" "integration_test_reboot" {
-  count = contains(local.reboot_required_tests, var.test_dir) ? 1 : 0
-
-  connection {
-    type        = "ssh"
-    user        = var.user
-    private_key = local.private_key_content
-    host        = aws_instance.cwagent.public_ip
-  }
-
-  # Prepare Integration Test
-  provisioner "remote-exec" {
-    inline = [
-      "echo reboot instance",
-      "sudo shutdown -r now &",
-    ]
-  }
-
-  depends_on = [
-    null_resource.integration_test_setup,
-  ]
-}
-
-resource "null_resource" "integration_test_wait" {
-  count = contains(local.reboot_required_tests, var.test_dir) ? 1 : 0
-  provisioner "local-exec" {
-    command = "echo Sleeping for 3m after initiating instance restart && sleep 180"
-  }
-  depends_on = [
-    null_resource.integration_test_reboot,
-  ]
-}
 
 resource "null_resource" "integration_test_setup" {
   connection {
     type        = "ssh"
     user        = var.user
-    private_key = local.private_key_content
-    host        = aws_instance.cwagent.public_ip
+    private_key = module.linux_common.private_key_content
+    host        = module.linux_common.cwagent_public_ip
   }
 
   # Prepare Integration Test.
@@ -186,12 +59,46 @@ resource "null_resource" "integration_test_setup" {
       "printf '\n[default]\nregion = us-west-2' | sudo tee -a ~/.aws/config",
       "printf '\n[default]\naws_access_key_id=%s\naws_secret_access_key=%s\naws_session_token=%s' $(aws sts assume-role --role-arn arn:aws:iam::506463145083:role/CloudWatchAgentServerRoleOnPrem --role-session-name test --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text) | sudo tee -a ~/.aws/credentials>/dev/null",
       "echo turning off imds access in order to make agent start with onprem mode",
-      "aws ec2 modify-instance-metadata-options --instance-id ${aws_instance.cwagent.id} --http-endpoint disabled",
+      "aws ec2 modify-instance-metadata-options --instance-id ${module.linux_common.cwagent_id} --http-endpoint disabled",
     ]
   }
 
   depends_on = [
-    null_resource.integration_test_fips_check,
+    module.linux_common,
+  ]
+}
+
+## reboot when only needed
+resource "null_resource" "integration_test_reboot" {
+  count = contains(local.reboot_required_tests, var.test_dir) ? 1 : 0
+
+  connection {
+    type        = "ssh"
+    user        = var.user
+    private_key = module.linux_common.private_key_content
+    host        = module.linux_common.cwagent_public_ip
+  }
+
+  # Prepare Integration Test
+  provisioner "remote-exec" {
+    inline = [
+      "echo reboot instance",
+      "sudo shutdown -r now &",
+    ]
+  }
+
+  depends_on = [
+    null_resource.integration_test_setup,
+  ]
+}
+
+resource "null_resource" "integration_test_wait" {
+  count = contains(local.reboot_required_tests, var.test_dir) ? 1 : 0
+  provisioner "local-exec" {
+    command = "echo Sleeping for 3m after initiating instance restart && sleep 180"
+  }
+  depends_on = [
+    null_resource.integration_test_reboot,
   ]
 }
 
@@ -199,8 +106,8 @@ resource "null_resource" "integration_test_run" {
   connection {
     type        = "ssh"
     user        = var.user
-    private_key = local.private_key_content
-    host        = aws_instance.cwagent.public_ip
+    private_key = module.linux_common.private_key_content
+    host        = module.linux_common.cwagent_public_ip
   }
 
   #Run sanity check and integration test
@@ -213,7 +120,7 @@ resource "null_resource" "integration_test_run" {
       "echo run integration test",
       "cd ~/amazon-cloudwatch-agent-test",
       "echo run sanity test && go test ./test/sanity -p 1 -v",
-      "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.proxy_instance.proxy_ip} -instanceId=${aws_instance.cwagent.id} -agentStartCommand='${var.agent_start}' -v",
+      "go test ${var.test_dir} -p 1 -timeout 1h -computeType=EC2 -bucket=${var.s3_bucket} -plugins='${var.plugin_tests}' -cwaCommitSha=${var.cwa_github_sha} -caCertPath=${var.ca_cert_path} -proxyUrl=${module.linux_common.proxy_instance_proxy_ip} -instanceId=${module.linux_common.cwagent_id} -agentStartCommand='${var.agent_start}' -v",
     ]
   }
 
@@ -221,13 +128,4 @@ resource "null_resource" "integration_test_run" {
     null_resource.integration_test_setup,
     null_resource.integration_test_wait,
   ]
-}
-
-data "aws_ami" "latest" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = [var.ami]
-  }
 }
